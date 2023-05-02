758bf9a845c7f6b0daf15515a9e6c0d1a61794bf
Nasir Mustapha <teboy.com@gmail.com>
2023-05-02 10:17:07 +0100

    FizzBuzz Correction

--- loops/assignment.py
+++ loops/assignment.py
@@ -6,3 +6,27 @@ but print “Fizz” if an integer is divisible by 3,
 and “FizzBuzz” if an integer is divisible 
 by both 3 and 5.
 """
+
+# for i in range(1, 16):
+#     if i % 5 == 0 and i % 3 == 0:
+#         print('FizzBuzz')
+#     elif i % 3 == 0:
+#         print('Fizz')
+#     elif i % 5 == 0:
+#         print('Buzz')
+#     else:
+#         print(i)
+
+
+i = 1
+
+while i < 16:
+    if i % 5 == 0 and i % 3 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+    i+=1
